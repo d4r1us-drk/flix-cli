4e4a00c46387c079eacc3e97365f0ca99631093b
d4r1us-drk <drakeofeknir@gmail.com>
2022-01-18 20:25:03 -0400

    fixed

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 # notflix install script
+cachedir="$HOME/.cache/notflix/"
+installdir="$HOME/.cache/notflix/installation/"
+bindir="$HOME/.local/bin"
 echo "Installing/updating notflix now"
-mkdir -p ~/.cache/notflix/
-mkdir -p ~/.cache/notflix/installation/
-mkdir -p ~/.local/bin/
+mkdir -p "$cachedir"
+mkdir -p "$installdir"
+mkdir -p "$bindir"
 npm install -g webtorrent-cli --prefix ~/.local/
-git clone https://github.com/d4r1us-drk/notflix ~/.cache/notflix/installation/ && cd ~/.cache/notflix/installation/
-cp notflix ~/.local/bin/
-rm -rf ~/.cache/notflix/installation/
+git clone https://github.com/d4r1us-drk/notflix "$installdir" && cd "$installdir"
+cp notflix "$bindir"
+rm -rf "$cachedir"
 echo "Notflix has been installed/updated"
