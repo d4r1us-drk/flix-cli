8ff09a00afc38711ed0bc1ce80567009c9494cc4
Clay Gomera <95720679+d4r1us-drk@users.noreply.github.com>
2022-04-13 11:08:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-cachedir="$HOME/.cache/notflix/"
+cachedir="$HOME/.cache/flix-cli/"
  echo -e "\e[38;5;$((RANDOM%257))m
-     __      _    __ _ _
-  /\ \ \___ | |_ / _| (_)_  __
- /  \/ / _ \| __| |_| | \ \/ /
-/ /\  / (_) | |_|  _| | |>  <   A simple script to watch movies from the terminal
-\_\ \/ \___/ \__|_| |_|_/_/\_\  Installing/Updating Notflix now!
-
+ 
+ ________ ___       ___     ___    ___             ________  ___       ___     
+|\  _____\\  \     |\  \   |\  \  /  /|           |\   ____\|\  \     |\  \    
+\ \  \__/\ \  \    \ \  \  \ \  \/  / /___________\ \  \___|\ \  \    \ \  \     A simple script to watch movies from the terminal
+ \ \   __\\ \  \    \ \  \  \ \    / /\____________\ \  \    \ \  \    \ \  \    Installing/Updating flix-cli now!
+  \ \  \_| \ \  \____\ \  \  /     \/\|____________|\ \  \____\ \  \____\ \  \ 
+   \ \__\   \ \_______\ \__\/  /\   \                \ \_______\ \_______\ \__\
+    \|__|    \|_______|\|__/__/ /\ __\                \|_______|\|_______|\|__|
+                           |__|/ \|__|                                         
   \e[0m"
 sudo npm install -g webtorrent-cli
-git clone https://github.com/d4r1us-drk/notflix.git "$cachedir"
+git clone https://github.com/d4r1us-drk/flix-cli.git "$cachedir"
 cd "$cachedir"
 sudo make install
 rm -rf "$cachedir"
