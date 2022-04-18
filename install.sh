df8072fd5c34acc67087f23576f85be29c26ee67
Clay Gomera <95720679+d4r1us-drk@users.noreply.github.com>
2022-04-18 17:03:12 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
-cachedir="$HOME/.cache/flix-cli/"
- echo -e "\e[38;5;$((RANDOM%257))m
+echo -e "\e[38;5;$((RANDOM%257))m
  
  ________ ___       ___     ___    ___             ________  ___       ___     
 |\  _____\\  \     |\  \   |\  \  /  /|           |\   ____\|\  \     |\  \    
@@ -12,11 +10,12 @@ cachedir="$HOME/.cache/flix-cli/"
     \|__|    \|_______|\|__/__/ /\ __\                \|_______|\|_______|\|__|
                            |__|/ \|__|                                         
   \e[0m"
+echo "Now installing webtorrent-cli"
+sleep 1
 sudo npm install -g webtorrent-cli
-git clone https://github.com/d4r1us-drk/flix-cli.git "$cachedir"
-cd "$cachedir"
+echo "Now installing the script"
+sleep 1
 sudo make install
-rm -rf "$cachedir"
 echo "
 
         Finished Installing/Updating!
