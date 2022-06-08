c7cc690ecf4aac4de93cc346c86dfe5c6930dbd2
Clay Gomera <misterclay@tutanota.com>
2022-06-07 20:53:43 -0400

    Updated

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 echo -e "\e[38;5;$((RANDOM%257))m
 
- ________ ___       ___     ___    ___             ________  ___       ___
-|\  _____\\  \     |\  \   |\  \  /  /|           |\   ____\|\  \     |\  \
-\ \  \__/\ \  \    \ \  \  \ \  \/  / /___________\ \  \___|\ \  \    \ \  \     A simple script to watch movies from the terminal
- \ \   __\\ \  \    \ \  \  \ \    / /\____________\ \  \    \ \  \    \ \  \    Installing/Updating flix-cli now!
-  \ \  \_| \ \  \____\ \  \  /     \/\|____________|\ \  \____\ \  \____\ \  \
-   \ \__\   \ \_______\ \__\/  /\   \                \ \_______\ \_______\ \__\
-    \|__|    \|_______|\|__/__/ /\ __\                \|_______|\|_______|\|__|
-                           |__|/ \|__|
+___________.__  .__                 _________ .__  .__
+\_   _____/|  | |__|__  ___         \_   ___ \|  | |__|
+ |    __)  |  | |  \  \/  /  ______ /    \  \/|  | |  |    A simple script to watch movies from the terminal
+ |     \   |  |_|  |>    <  /_____/ \     \___|  |_|  |    Installing/Updating flix-cli now!
+ \___  /   |____/__/__/\_ \          \______  /____/__|
+     \/                  \/                 \/
+
   \e[0m"
-echo "Now installing webtorrent-cli"
+echo "Now installing peerflix"
 sleep 1
 sudo npm install -g peerflix
 echo "Now installing the script"
